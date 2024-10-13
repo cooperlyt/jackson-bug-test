99e1c912ccc9294c2c09279e6b1c82c9f38d470f
cooper <lyt3131@163.com>
2024-10-13 12:06:34 +0800

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "io.github.cooperlyt"
@@ -10,7 +13,27 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+
+
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    
+//    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.0")
+
+//    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.0")
+//
+//    implementation("com.fasterxml.jackson.core:jackson-core:2.18.0")
+//    implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.0")
+//    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
--- src/test/kotlin/JsonTest.kt
+++ src/test/kotlin/JsonTest.kt
@@ -1,2 +1,43 @@
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.cooperlyt.Application
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [Application::class])
 class JsonTest {
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    data class SimpleStockFreezeStatusMessage(
+         val businessId: Long,
+         val success: Boolean,
+         val tenantId: String,
+    )
+
+    @Test
+    fun testSpringJsonMessage() {
+
+        val json = """{"businessId":1336504106360835,"tenantId":"first","success":false}"""
+        val value = objectMapper.readValue(json, SimpleStockFreezeStatusMessage::class.java)
+
+        println("message ser by readValue : $value")
+        assert(value.tenantId == "first")
+
+        val view: Class<*> = SimpleStockFreezeStatusMessage::class.java
+        val javaType: JavaType = objectMapper.constructType(object : TypeReference<SimpleStockFreezeStatusMessage>() {})
+
+        //this is Spring message Usage
+        val value2 = objectMapper.readerWithView(view).forType(javaType).readValues<Any>(json).nextValue()
+        println("message ser by readerWithView: $value2")
+
+        assertEquals((value2 as SimpleStockFreezeStatusMessage).tenantId, "first")
+    }
 }
