85393be96c62b4deb8889e542ba307e840a1fe76
cooper <lyt3131@163.com>
2024-10-14 09:33:07 +0800

    init

--- java-test/src/test/java/json/JsonTest.java
+++ java-test/src/test/java/json/JsonTest.java
@@ -1,25 +1,34 @@
 package json;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import io.github.cooperlyt.Main;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponentModule;
+import org.springframework.boot.jackson.JsonMixinModule;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith({SpringExtension.class})
 @SpringBootTest(classes = Main.class)
 public class JsonTest {
 
-  @Autowired
-  private  ObjectMapper objectMapper;
+
 
   static class SimpleStockFreezeStatusMessage {
 
@@ -53,6 +62,20 @@ public class JsonTest {
   }
 
   public void testSpringJsonMessage(ObjectMapper objectMapper) throws IOException {
+
+    System.out.println("Registered modules: " + objectMapper.getRegisteredModuleIds());
+    System.out.println("Deserialization features: ");
+    for (DeserializationFeature feature : DeserializationFeature.values()) {
+      boolean isEnabled = objectMapper.getDeserializationConfig().isEnabled(feature);
+      System.out.println(feature + ": " + isEnabled);
+    }
+
+    System.out.println("Serialization features: ");
+    for (SerializationFeature feature : SerializationFeature.values()) {
+      boolean isEnabled = objectMapper.getSerializationConfig().isEnabled(feature);
+      System.out.println(feature + ": " + isEnabled);
+    }
+
     var json = "{\"businessId\":1336504106360835,\"tenantId\":\"first\",\"success\":false}";
     var value = objectMapper.readValue(json, SimpleStockFreezeStatusMessage.class);
 
@@ -71,15 +94,46 @@ public class JsonTest {
     assertEquals(value2.tenantId, "first");
   }
 
+
+  @Autowired
+  private  ObjectMapper objectMapper;
+
+  //Fails
+  @Test
+  public void testBySpringMapper() throws IOException {
+    testSpringJsonMessage(objectMapper);
+  }
+
+  @Autowired
+  private Jackson2ObjectMapperBuilder objectMapperBuilder;
+
+  //Fails
+  @Test
+  public void testBySpringMapperBuilder() throws IOException {
+    testSpringJsonMessage(objectMapperBuilder.build());
+  }
+
+  //Success
   @Test
   public void testByDefaultMapper() throws IOException {
     testSpringJsonMessage(new ObjectMapper());
   }
 
-
+  //Success
   @Test
-  public void testBySpringMapper() throws IOException {
-    testSpringJsonMessage(objectMapper);
+  public void testMockSpringMapper() throws IOException {
+    var ob = new ObjectMapper();
+    ob.registerModule(new Jdk8Module());
+    ob.registerModule(new ParameterNamesModule());
+    ob.registerModule(new JSR310Module());
+    ob.registerModule(new JsonMixinModule());
+    ob.registerModule(new JsonComponentModule());
+
+    ob = ob.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    ob = ob.configure(WRITE_DATES_AS_TIMESTAMPS, false);
+    ob = ob.configure(WRITE_DURATIONS_AS_TIMESTAMPS, false);
+    testSpringJsonMessage(ob);
   }
 
 
